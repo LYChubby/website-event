87e6e1d2f7568ef4fa9166ddae8973362f32803b
LYChubby <f4uzi345@gmail.com>
2025-07-23 15:15:36 +0700

    Membuat Model Event

---
+++ app/Models/Event.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'event_id';
+
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'name_event',
+        'description',
+        'event_image',
+        'venue_name',
+        'venue_address',
+        'start_date',
+        'end_date',
+        'status_approval',
+    ];
+
+    // Relasi ke User (Organizer)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relasi ke Kategori
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    // Relasi ke Ticket
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class, 'event_id');
+    }
+
+    // Relasi ke Transaksi
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class, 'event_id');
+    }
+}
