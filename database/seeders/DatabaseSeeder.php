8dc1f7000d6803f51fab7fd864a2ab0751a024aa
muhfarrasm <muhammadfarrasm@gmail.com>
2025-07-24 10:06:46 +0700

    ADD - Seed admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,24 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+   public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Buat 1 admin
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'role' => 'admin', // Pastikan ada kolom role di tabel users
+            'password' => Hash::make('admin123'), // Ganti password sesuai kebutuhan
         ]);
+
+        // Kalau mau user dummy tambahan:
+        // User::factory(10)->create();
     }
 }
