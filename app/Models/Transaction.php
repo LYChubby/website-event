1f0400562bba5281a5f84b56dbf7543af434c078
LYChubby <f4uzi345@gmail.com>
2025-07-31 13:32:09 +0700

    Model Transaction Sementara

---
+++ app/Models/Transaction.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $table = 'transactions';
+
+    protected $primaryKey = 'transaction_id';
+
+    protected $fillable = [
+        'user_id',
+        'event_id',
+        'no_invoice',
+        'total_price',
+        'status_pembayaran',
+        'payment_method',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class, 'event_id', 'event_id');
+    }
+}
