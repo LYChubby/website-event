4c509ea35f4981352b51c9800fc2de4f5ed19691
Farras <126749150+muhfarrasm@users.noreply.github.com>
2025-08-02 14:58:46 +0700

    muah

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -13,12 +13,17 @@ class HistoryController extends Controller
     /**
      * Menampilkan daftar riwayat pembelian tiket untuk user yang login.
      */
+
+
     public function index()
     {
         $user = Auth::user();
+        if (!$user) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
 
         $query = Participant::with(['event', 'ticket'])
-            ->where('user_id', $user->id)
+            ->where('participants.user_id', $user->id) // PERJELAS disini
             ->join('transactions', 'participants.user_id', '=', 'transactions.user_id')
             ->select(
                 'participants.nama as nama_pembeli',
@@ -48,6 +53,42 @@ class HistoryController extends Controller
 
         return response()->json($riwayat);
     }
+
+    // public function index()
+    // {
+    //     $user = Auth::user();
+
+    //     $query = Participant::with(['event', 'ticket'])
+    //         ->where('user_id', $user->id)
+    //         ->join('transactions', 'participants.user_id', '=', 'transactions.user_id')
+    //         ->select(
+    //             'participants.nama as nama_pembeli',
+    //             'participants.name_event',
+    //             'transactions.created_at as tanggal_beli',
+    //             'transactions.status_pembayaran',
+    //             'transactions.transaction_id'
+    //         )
+    //         ->groupBy(
+    //             'transactions.transaction_id',
+    //             'participants.nama',
+    //             'participants.name_event',
+    //             'transactions.created_at',
+    //             'transactions.status_pembayaran'
+    //         )
+    //         ->orderBy('transactions.created_at', 'desc');
+
+    //     $riwayat = $query->get()->map(function ($item) {
+    //         return [
+    //             'transaction_id' => $item->transaction_id,
+    //             'nama_pembeli' => $item->nama_pembeli,
+    //             'nama_event' => $item->name_event,
+    //             'status_pembayaran' => $item->status_pembayaran,
+    //             'tanggal_beli' => $item->tanggal_beli,
+    //         ];
+    //     });
+
+    //     return response()->json($riwayat);
+    // }
 
     /**
      * Menampilkan detail transaksi berdasarkan ID transaksi.
